e3fcaf11a1b34bc3e22bd0003c12b05451cccbae
Joey Luis Echevarria <jechevarria719@gmail.com>
2019-09-14 20:46:24 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,9 @@
 def oxford_comma(array)
-  if array.length == 2
-    return "#{array[0]} and #{array[1]}"
-  elsif 2 < array.length
-    array[-1].insert(0, "and ")
-  end
-  array.join(",")
+  case array.length
+when 1 
+  "#{array[0]}"
+when 2 
+  array[0..1].join("and")
+else
+  array[0...1].join(",") <<", and #{array[-1]}"
 end
